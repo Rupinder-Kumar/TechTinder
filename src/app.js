ee1dece7d5364cb0dc6337b8b4d8b7d0dd134ac2
Rupinder <rupinder3972@gmail.com>
2026-06-09 12:49:16 +0530

    Initialized node project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test",(req, res) => {
+        res.send("hello from server");
+});
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000.....");
+});
+
