9e2ed7c738ac3d1ecba9e038ffe20ee1601e4f19
Rupinder <rupinder3972@gmail.com>
2026-06-11 18:52:15 +0530

    Removed DB string from git

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 
-    await mongoose.connect("mongodb+srv://rupinder7298_db_user:R7KugJrCb2bq0XY4@nodeproject.o66bjng.mongodb.net/TechTinder")
 }
 
 module.exports = connectDB;
