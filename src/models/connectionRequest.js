0c4fd4bf631ba1876b25478a694f209455b230a4
Rupinder <rupinder3972@gmail.com>
2026-06-17 17:18:32 +0530

    Added indexing for connection request collection

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -19,6 +19,7 @@ const connectionRequestSchema = new mongoose.Schema({
         }
 },{timestamps: true});
 
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
 connectionRequestSchema.pre("save", async function() {
     const connectionRequest = this;
